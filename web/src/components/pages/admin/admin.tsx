81a6fa6dd0b9223c32fd89711da6141bfbefff9a
Mohamed SAMB <mohamed.samb@baamtu.com>
2020-11-06 08:32:08 +0000

    expandable row for user details init

--- web/src/components/pages/admin/admin.tsx
+++ web/src/components/pages/admin/admin.tsx
@@ -9,6 +9,15 @@ import StateTree from '../../../stores/tree';
 import URLS from '../../../urls';
 import DataTable from 'react-data-table-component';
 
+// The row data is composed into your custom expandable component via the data prop
+const ExpandableComponent = (data: any) => (
+  <div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia unde eum
+    alias possimus esse non odio ad vero dignissimos nesciunt? Cupiditate omnis
+    laudantium facilis sunt, temporibus laboriosam ad iste eaque.
+  </div>
+);
+
 let data: [any];
 const API_HOST = 'http://localhost:9000/api/v1';
 const data2 = fetch(API_HOST + '/all_users')
@@ -53,5 +62,13 @@ const columns = [
 import './admin.css';
 
 export default function Admin() {
-  return <DataTable title="Utilisateurs" columns={columns} data={data} />;
+  return (
+    <DataTable
+      title="Utilisateurs"
+      columns={columns}
+      data={data}
+      expandableRows
+      expandableRowsComponent={<ExpandableComponent />}
+    />
+  );
 }
