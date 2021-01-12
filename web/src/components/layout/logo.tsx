c9080c8a04b146a89f8ded16bfa5857cd52205e4
Mohamed SAMB <mohamed.samb@baamtu.com>
2021-01-12 14:42:37 +0000

    integrate Baamtu logo in the UI

--- web/src/components/layout/logo.tsx
+++ web/src/components/layout/logo.tsx
@@ -3,10 +3,12 @@ import { LocaleLink } from '../locale-helpers';
 
 export default (props: { reverse?: boolean }) => {
   const imgSrc = require('./galsen-ai.png');
+  const corpBuilder = require('./logo_baamtu_web.png');
 
   return (
     <LocaleLink className="main-logo" to="">
       <img className="main-mozilla-logo" src={imgSrc} />
+      <img className="main-mozilla-logo" src={corpBuilder} />
     </LocaleLink>
   );
 };
